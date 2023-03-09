cb6a392c6619189144e71a2ca69a6306f7afe1e1
Tan Le <tanld.dev@gmail.com>
2023-03-09 11:59:32 +0700

    added in our addToCart feature
    :q
    /

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,13 +1,56 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
+
+const addCartItem = (cartItems, productToAdd) => {
+  // find if cartItems contains productToAdd
+  const existingCartItem = cartItems.find(
+    (item) => item.id === productToAdd.id
+  );
+  // if found, increment quantity,
+  if (existingCartItem) {
+    return cartItems.map(
+      // Find the exitsting item, and increment its quantity value.
+      (item) =>
+        item.id === productToAdd.id
+          ? { ...item, quantity: item.quantity + 1 }
+          : item
+    );
+  }
+  // return new array with modified cartItems / new cart item
+  return [...cartItems, { ...productToAdd, quantity: 1 }];
+};
 
 export const CartContext = createContext({
   isCartOpen: false,
   setIsCartOpen: () => {},
+  cartItems: [],
+  addItemToCart: () => {},
 });
 
 export const CartProvider = ({ children }) => {
   const [isCartOpen, setIsCartOpen] = useState(false);
-  const value = { isCartOpen, setIsCartOpen };
+  const [cartItems, setCartItems] = useState([]);
+  const [cartCount, setCartCount] = useState(0);
+
+  useEffect(() => {
+    // calculate the cart item count whenever the cartItems state change.
+    const newCartCount = cartItems.reduce(
+      (total, cartItem) => total + cartItem.quantity,
+      0
+    );
+    setCartCount(newCartCount);
+  }, [cartItems]);
+
+  const addItemToCart = (productToAdd) => {
+    setCartItems(addCartItem(cartItems, productToAdd));
+  };
+
+  const value = {
+    isCartOpen,
+    setIsCartOpen,
+    addItemToCart,
+    cartItems,
+    cartCount,
+  };
 
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
