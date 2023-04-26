cf117413c5794685f8f9790c14756aa210883914
Tan Le <tanld.dev@gmail.com>
2023-04-26 15:58:26 +0700

    [TS] Typing Categories state; Finish typing Categories reducer

--- src/store/categories/categories.reducer.ts
+++ src/store/categories/categories.reducer.ts
@@ -1,10 +1,15 @@
-import { CATEGORIES_ACTION_TYPES } from "./categories.types";
+import { CATEGORIES_ACTION_TYPES, Category } from "./categories.types";
 
 import { CategoryAction } from "./categories.action";
 
+export type CategoriesState = {
+  readonly categories: Category[];
+  readonly isLoading: boolean;
+  readonly error: Error | null;
+};
+
 export const CATEGORIES_INITIAL_STATE = {
   categories: [],
-  // for async redux
   isLoading: false,
   error: null,
 };
@@ -13,15 +18,13 @@ export const categoriesReducer = (
   state = CATEGORIES_INITIAL_STATE,
   action = {} as CategoryAction
 ) => {
-  const { type, payload } = action;
-
-  switch (type) {
+  switch (action.type) {
     case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START:
       return { ...state, isLoading: true };
     case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS:
-      return { ...state, categories: payload, isLoading: false };
+      return { ...state, categories: action.payload, isLoading: false };
     case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED:
-      return { ...state, error: payload, isLoading: false };
+      return { ...state, error: action.payload, isLoading: false };
     default:
       return state;
   }
